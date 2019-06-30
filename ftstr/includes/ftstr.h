bd201cf9eb4f0904391c64c8fbfcaef40ceee35f
OlegMulko <oleg.mylko@mail.ru>
2019-06-30 13:49:38 +0300

    Mod function ft_get_cnt_words
    * size_t  ft_get_cnt_words(const char *str, char spr);
    * rename file function ft_is_array_contain => ft_is_str_contain

--- ftstr/includes/ftstr.h
+++ ftstr/includes/ftstr.h
@@ -5,7 +5,7 @@
 # include <stdlib.h>
 # include "libft.h"
 
-size_t  ft_get_cnt_words(const char *str, char spr);
+size_t  ft_get_cnt_words(const char *str, char *spr);
 char    *ft_last_word_pos(const char *str, char *seps);
 int     ft_is_str_contain(char *str, char sym);
 
