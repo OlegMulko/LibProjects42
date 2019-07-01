553140bf924afb83d8ced64939d84a0adc47bd66
Oleg <oleg.mylko@mail.ru>
2019-07-01 14:17:44 +0300

    Fix function: ft_match

--- ftstr/srcs/ft_match.c
+++ ftstr/srcs/ft_match.c
@@ -7,11 +7,11 @@ int	ft_match(char *s1, char *s2)
 	else if (!*s1 && !*s2)
 		return (1);
 	else if (*s1 != '\0' && *s2 == '*')
-		return (match(s1 + 1, s2) || match(s1, s2 + 1));
+		return (ft_match(s1 + 1, s2) || ft_match(s1, s2 + 1));
 	else if(*s1 == '\0' && *s2 == '*')
-		return (match(s1, s2 + 1));
+		return (ft_match(s1, s2 + 1));
 	else if (*s1 == *s2)
-		return (match(s1 + 1, s2 + 1));
+		return (ft_match(s1 + 1, s2 + 1));
 	else
 		return (0);
 }
