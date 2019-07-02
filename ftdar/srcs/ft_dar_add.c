3337438070c4381e0588b17d636f4415b63ab192
Guy Grimes <ggrimes@at-l5.21-school.ru>
2019-07-02 23:48:50 +0300

    Mod function in lib ftdar: ft_dar_add
    * ft_strarr_len => ft_dar_size

--- ftdar/srcs/ft_dar_add.c
+++ ftdar/srcs/ft_dar_add.c
@@ -3,35 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_dar_add.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ggrimes <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: ggrimes <ggrimes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 23:10:29 by ggrimes           #+#    #+#             */
-/*   Updated: 2019/07/02 23:14:06 by ggrimes          ###   ########.fr       */
+/*   Updated: 2019/07/02 23:48:25 by ggrimes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ftdar.h"
 
-char	**ft_strarr_add(char **dest, char **src, char **exc)
+char	**ft_dar_add(char **dest, char **src, char **exc)
 {
 	int		i;
 	int		j;
-	char	**str_arr;
+	char	**dar;
 	int		len;
 
 	if (!src && !*src)
 		return (NULL);
-	len = ft_strarr_len(dest);
-	len += ft_strarr_len(src);
-	CHECK((str_arr = (char **)malloc(sizeof(char *) * len + 1)));
+	len = ft_dar_size(dest);
+	len += ft_dar_size(src);
+	CHECK((dar = (char **)malloc(sizeof(char *) * len + 1)));
 	i = (dest) ? -1 : 0;
 	if (dest)
 		while (dest[++i])
-			str_arr[i] = ft_strdup(dest[i]);
+			dar[i] = ft_strdup(dest[i]);
 	j = -1;
 	while (src[++j])
 		if (!ft_cheak_exc(src[j], exc))
-			str_arr[i++] = ft_strdup(src[j]);
-	str_arr[i] = NULL;
-	return (str_arr);
+			dar[i++] = ft_strdup(src[j]);
+	dar[i] = NULL;
+	return (dar);
 }
