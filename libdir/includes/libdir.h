8641622277c1fbb36c8992e053d266b37952bfb9
Adolfo Ashara dayne <aashara-@ox-s4.21-school.ru>
2019-10-23 16:19:47 +0300

    Add ft_file_type

--- libdir/includes/libdir.h
+++ libdir/includes/libdir.h
@@ -6,7 +6,7 @@
 /*   By: aashara- <aashara-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/03 00:01:35 by ggrimes           #+#    #+#             */
-/*   Updated: 2019/09/15 21:22:57 by aashara-         ###   ########.fr       */
+/*   Updated: 2019/10/23 16:17:12 by aashara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,10 @@
 # include <stdlib.h>
 # include "libft.h"
 # include <dirent.h>
+# include <sys/stat.h>
 
 size_t	ft_size_dir(char *dirpath);
 short	ft_dir_flag(short flag, unsigned char d_type);
 char	**ft_dir_content(char *dirpath, short flag);
-
+char	ft_file_type(char *path);
 #endif
