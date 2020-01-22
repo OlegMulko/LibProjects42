143d6e5aec786d5b4969ba459251c9ece9aeef64
fica99 <fica.c@yandex.ru>
2020-01-22 21:51:17 +0300

    Update ft_dar_sort

--- libdar/srcs/ft_dar_sort.c
+++ libdar/srcs/ft_dar_sort.c
@@ -6,37 +6,29 @@
 /*   By: aashara- <aashara-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/21 18:24:23 by aashara-          #+#    #+#             */
-/*   Updated: 2020/01/21 18:27:52 by aashara-         ###   ########.fr       */
+/*   Updated: 2020/01/22 21:50:27 by aashara-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libdar.h"
 
-void	ft_dar_quick_sort(char **res, size_t size)
+void	ft_dar_quick_sort(char **res)
 {
 	size_t	i;
-	size_t	j;
 	char	*tmp;
 
-	if (size < 2)
-		return ;
 	i = 0;
-	j = size - 1;
-	while (i <= j)
+	while (res[i + 1])
 	{
-		while (ft_strcmp(res[i], res[size / 2]) < 0)
-			++i;
-		while (ft_strcmp(res[j], res[size / 2]) > 0)
-			--j;
-		if (i <= j)
+		if (ft_strcmp(res[i], res[i + 1]) > 0)
 		{
 			tmp = res[i];
-			res[i++] = res[j];
-			res[j--] = tmp;
+			res[i] = res[i + 1];
+			res[i + 1] = tmp;
+			if (i != 0)
+				--i;
+			continue ;
 		}
+		++i;
 	}
-	if (j > 0)
-		ft_dar_quick_sort(res, j);
-	if (size > i)
-		ft_dar_quick_sort(res + i, size - i);
 }
